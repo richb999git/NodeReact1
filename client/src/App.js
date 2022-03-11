eaa39bef8a60625e97e77cc0d4c9514c9c0cced3
Richard Braunton <richard@computech.software>
2022-03-11 15:44:13 +0000

    Add Name form and handlers to display greeting to user. Update where api is pointing for original message.

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,50 @@ import "./App.css";
 
 function App() {
   const [data, setData] = React.useState(null);
+  const [name, setName] = React.useState('');
+  const [greeting, setGreeting] = React.useState('');
 
   React.useEffect(() => {
-    fetch("/api")
+    fetch("/api/test")
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
 
+  const handleChange = (value) => {
+      setName(value);
+  }
+
+  const handleSubmit = (event) => {
+      event.preventDefault();
+      setName('')
+
+      fetch(`/api/greeting?name=${encodeURIComponent(name)}`)
+          .then(response => response.json())
+          .then(jsonResponse => setGreeting(jsonResponse.greeting))
+
+      // Alternative using await (async needed at function top level)
+      // const respose = await fetch(`/api/greeting?name=${encodeURIComponent(name)}`)
+      // const jsonResponse = await respose.json()
+      // setGreeting(jsonResponse.greeting)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>{!data ? "Loading..." : data}</p>
+        </header>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Enter your name: </label>
+            <input
+              id="name"
+              type="text"
+              value={name}
+              onChange={event => handleChange(event.target.value)}
+            />
+            <button type="submit">Submit</button>
+        </form>
+        <p>Greeting: {greeting}</p>
     </div>
   );
 }
